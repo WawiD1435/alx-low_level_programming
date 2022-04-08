1b0cfea6e47c131f293174d068d43e442a9c5ac3
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-08 13:28:11 -0700

    print all possible combination of two digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,7 +23,7 @@ int main(void)
 					putchar(t);
 					putchar(o);
 
-					if ((tens != 9 && ones != 8) && (t != 9 && o != 9))
+					if ((tens != 57 && ones != 56) && (t != 57 && o != 57))
 					{
 						putchar(',');
 						putchar(' ');
