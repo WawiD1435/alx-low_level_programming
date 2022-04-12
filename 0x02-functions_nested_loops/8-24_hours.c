d29ca4c9bb197ca2b03f64938458209eccc7ead6
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-12 06:47:04 -0700

    print every minute of a day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * jack_bauer- prints every minute of a day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hrs, mins;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar(hrs / 10 + '0');
+			_putchar(hrs % 10 + '0');
+			_putchar(':');
+			_putchar(mins / 10 + '0');
+			_putchar(mins % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
