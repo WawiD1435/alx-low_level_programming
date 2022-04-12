0a88f2c1594e5a0a3f1c368b9010fc4f3bb0fef0
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-12 07:30:54 -0700

    times table upto 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 {
 	int row, column;
 
-	for (int row = 0; row <= 9; row++)
+	for (row = 0; row <= 9; row++)
 	{
-		for (int column = 0; column <= 9; column++)
+		for (column = 0; column <= 9; column++)
 		{
 			int product = row * column;
 			int tens = product / 10;
