5d0fd348b0ef5e9e94a6b851e2e88ad1d1336045
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-12 08:39:08 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,26 +13,30 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (i=n; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
-			printf("%d", i);
 			if (n != 98)
 			{
+				printf("%d", i);
 				printf(",");
 				printf(" ");
 			}
+			else
+				printf("%d", i);
 		}
 	}
 	else
 	{
-		for (i=n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
 			if (n != 98)
 			{
+				printf("%d", i);
 				printf(",");
 				printf(" ");
 			}
+			else
+				printf("%d", i);
 		}
 	}
 }
