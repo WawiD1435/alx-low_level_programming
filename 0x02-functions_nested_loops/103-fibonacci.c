1a6acf8152f9909273b96e1b3151392f884c37c3
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-12 16:48:22 -0700

    sum of even valued fibonacci series

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: print sum of even valued terms of fibonnaci series
+ *
+ * Return: 0(sucess);
+ */
+
+int main(void)
+{
+	int i = 1, int j = 2, k = 0, total = 0;
+
+	while (j < 4000000)
+	{
+		if (j % 2 == 0)
+			total += j;
+		k = j;
+		j += i;
+		i = k;
+	}
+	printf("%D\n", total);
+	return (0);
+}
+
