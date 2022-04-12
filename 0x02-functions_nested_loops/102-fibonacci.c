c2f82d4ada32cd093765d6cc562b8f0e1be8ab2c
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-12 16:24:54 -0700

    first 50 fibonnaci series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Description: prints first 50 fibonnaci numbers
+ *
+ * Return: 0(suces)
+ */
+int main(void)
+{
+	unsigned long int count = 50, first_term = 1, second_term = 2, next_term = 0;
+	int i;
+
+	printf("%lu, %lu, ", first_term, second_term);
+	for (i = 3; i < count; i++)
+	{
+		next_term = first_term + second_term;
+		first_term = second_term;
+		second_term = next_term;
+
+		if (i > 2 && i < 50)
+			printf("%lu, ", next_term);
+		else
+			printf("%lu\n", next_term);
+	}
+	return (0);
+}
+
