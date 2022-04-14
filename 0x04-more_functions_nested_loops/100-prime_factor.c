4ad528db58ca0a52ea06c0a3f12903ef3cb697d0
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-14 10:40:10 -0700

    larggest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-	unsigned long int x = 612852475143, max , i;
+	unsigned long int x = 612852475143, max, i;
 
 	while (x % 2 == 0)
 	{
 		max = 2;
 		x /= 2;
 	}
-	for (i = 3; i <= x / 2; i+=2)
+	for (i = 3; i <= x / 2; i += 2)
 	{
 		while (x % i == 0)
 		{
