fedd8fad302b308c6ba028fd9ddfce22c12929d9
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-14 10:32:05 -0700

    larggest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main- prints the largest prime factor number
+ * Return:0
+ */
+
+int main(void)
+{
+	unsigned long int x = 612852475143, max , i;
+
+	while (x % 2 == 0)
+	{
+		max = 2;
+		x /= 2;
+	}
+	for (i = 3; i <= x / 2; i+=2)
+	{
+		while (x % i == 0)
+		{
+			max = i;
+			x /= i;
+		}
+	}
+	if (x > 2)
+	{
+		max = x;
+	}
+	printf("%ld\n", max);
+
+	return (0);
+}
+
+
