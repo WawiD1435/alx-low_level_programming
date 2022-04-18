8a1cf01954e5da870d60dbcb248455b2d222e825
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-18 11:59:06 -0700

    change string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,14 +8,14 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, resp = 0, first_num;
+	int sign = 1, resp = 0, first_num, i;
 
 	for (first_num = 0; !(s[first_num] >= 48 && s[first_num] <= 57); first_num++)
 	{
 		if (s[first_num == '-'])
 			sign *= -1;
 	}
-	for (int i = first_num; s[i] >= 48 && s[i] <= 57; i++)
+	for (i = first_num; s[i] >= 48 && s[i] <= 57; i++)
 	{
 		resp *= 10;
 		resp += (s[i] - 48);
