968d9c830a3383c18acf18e8cf983ceacf483cfa
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-18 08:21:39 -0700

    prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints in reverse
+ * @s: string parameter
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int count;
+
+	while (*(s + count != 0))
+		count++;
+	for (count--; count >= 0; count--)
+		_putchar(*(s + count));
+}
+
