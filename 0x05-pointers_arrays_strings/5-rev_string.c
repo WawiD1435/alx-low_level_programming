341ec03b086c3eefc033894e65134220f28c80d7
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-18 09:05:26 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _strlen - returns length of string
+ * @s: string
+ * Return: length of characters
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != 0)
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+
+/**
  * rev_string - recerses a string
  * @s: string
  * Return: void
