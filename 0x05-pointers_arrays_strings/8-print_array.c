f214d037558942eee4520eccb644a46a324d27d5
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-18 10:55:11 -0700

    prints array of elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - prints an array
+ * @a: array of integers
+ * @n: number of elements of array
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
