5a6e74e131c396ceaa0df2fb1c2129ceb23e0f90
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-21 04:59:01 -0700

    string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,7 +13,7 @@ char *string_toupper(char *str)
 	i = 0;
 	while (*(str + i) != '\0')
 	{
-		if (*(str + i) > 91 && *(str + i) < 122)
+		if (*(str + i) >= 97 && *(str + i) <= 122)
 			*(str + i) -= 32;
 		i++;
 	}
