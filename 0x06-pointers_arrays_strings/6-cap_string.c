cbc15e5b75b89540bd43143f53b54b419cbcde6f
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-21 05:19:36 -0700

    capitalize words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words of string
+ * @s: string
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	if (*(s + i) >= 97 && *(s + i) <= 122)
+		*(s + i) -= 32;
+	i++;
+	while (*(s + i) != '\0')
+	{
+		if ((*(s + i) >= 97 && *(s + i) <= 122)
+		&& (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+			|| s[i - 1] == '\n'))
+			*(s + i) -= 32;
+		i++;
+	}
+	return (s);
