0bebf1606cb466362d07c9fea241c36522c38f6e
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-04-29 00:20:20 -0700

    name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- print name of program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
