755b5e9c850c23e09b2b865a830b2eb063b664a9
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-05-06 19:23:04 -0700

    updated

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,92 +1,105 @@
-#include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
-/**
- * _isnumber - checks if string is number
- *
- * @s: string
- *
- * Return: 1 if number, 0 if not
- */
-int _isnumber(char *s)
-{
-	int i, check, d;
+#include "main.h"
 
-	d = 0, check = 1;
-	for (i = 0; *(s + i) != 0; i++)
+/**
+ * _isdigit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int _isdigit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
 	{
-		d = isdigit(*(s + i));
-		if (d == 0)
-		{
-			check = 0;
-			break;
-		}
+		if (s[i] < 48 || s[i] > 57)
+			return (0);
+		i++;
 	}
-	return (check);
+	return (1);
 }
 
 /**
- * _callocX - reserves memory initialized to 0
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * @nmemb: # of bytes
- *
- * Return: pointer
+ * Return: the length of the string
  */
-char *_callocX(unsigned int nmemb)
+int _strlen(char *s)
 {
-	unsigned int i;
-	char *p;
+	int i = 0;
 
-	p = malloc(nmemb + 1);
-	if (p == 0)
-		return (0);
-	for (i = 0; i < nmemb; i++)
-		p[i] = '0';
-	p[i] = '\0';
-	return (p);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
- * main - multiplies inf numbers
- *
- * @argc: # of cmd line args
- * @argv: cmd line args
- * Return: No return
+ * print - print Error, followed by a new line,
+ * and exit with a status of 98
  */
-int main(int argc, char **argv)
+void print(void)
 {
-	int i, j, l1, l2, lful, mul, add, ten, ten2, tl, zer = 0;
-	char *res;
+	char *str = "Error";
 
-	if (argc != 3 || _isnumber(argv[1]) == 0 || _isnumber(argv[2]) == 0)
-		printf("Error\n"), exit(98);
-	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
-		printf("0\n"), exit(0);
-	l1 = strlen(argv[1]), l2 = strlen(argv[2]);
-	lful = l1 + l2;
-	res = _callocX(lful);
-	if (res == 0)
-		printf("Error\n"), exit(98);
-	for (i = l2 - 1; i >= 0; i--)
+	while (*str != '\0')
 	{
-		ten = 0, ten2 = 0;
-		for (j = l1 - 1; j >= 0; j--)
-		{
-			tl = i + j + 1;
-			mul = (argv[1][j] - '0') * (argv[2][i] - '0') + ten;
-			ten =  mul / 10;
-			add = (res[tl] - '0') + (mul % 10) + ten2;
-			ten2 = add / 10;
-			res[tl] = (add % 10) + '0';
-		}
-		res[tl - 1] = (ten + ten2) + '0';
+		_putchar(*str);
+		str++;
 	}
-	if (res[0] == '0')
-		zer = 1;
-	for (; zer < lful; zer++)
-		printf("%c", res[zer]);
-	printf("\n");
-	free(res);
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *num1, *num2;
+	int len1, len2, len, i, carry, digit1, digit2, *mul, a = 0;
+
+	num1 = argv[1], num2 = argv[2];
+	if (argc != 3 || !_isdigit(num1) || !_isdigit(num2))
+		print();
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	len = len1 + len2;
+	mul = malloc(sizeof(int) * len);
+	if (mul == NULL)
+		return (1);
+	for (i = 0; i < len1 + len2; i++)
+		mul[i] = 0;
+	for (len1 = _strlen(num1) - 1; len1 >= 0; len1--)
+	{
+		digit1 = num1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(num2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = num2[len2] - '0';
+			carry += mul[len1 + len2 + 1] + (digit1 * digit2);
+			mul[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			mul[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (mul[i])
+			a = 1;
+		if (a)
+			_putchar(mul[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(mul);
 	return (0);
 }
