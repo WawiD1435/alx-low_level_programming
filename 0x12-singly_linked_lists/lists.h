32df08d8ba7ddba2c36c36d60afb2ba6387fd49d
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-05-19 11:09:40 -0700

    print list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly listed linked
+ * @str: string
+ * @len: length of string
+ * @next: points to next node
+ *
+ * Description: singly linked structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif
