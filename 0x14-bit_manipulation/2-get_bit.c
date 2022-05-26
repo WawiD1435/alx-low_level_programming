3565d3be77770632c0c98e494af4907d217d585c
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-05-26 07:09:02 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,12 +10,18 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int bits;
+	unsigned int i;
 
-	bits = (n >> index) & 1;
+	if (n == 0 && index < 64)
+		return (0);
 
-	if ((bits == 1) || (bits == 0))
-		return (bits);
-	else
-		return (-1);
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+
+	return (-1);
 }
