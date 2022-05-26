a7a8cb43b04e759ed9a4810849b330c45db04d1e
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-05-26 07:16:28 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,14 +11,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int bits;
+	unsigned int m;
 
-	if (n == NULL)
+	if (index > 63)
 		return (-1);
 
-	bits = 1 << index;
-
-	*n = *n | bits;
+	m = 1 << index;
+	*n = (*n | m);
 
 	return (1);
 }
